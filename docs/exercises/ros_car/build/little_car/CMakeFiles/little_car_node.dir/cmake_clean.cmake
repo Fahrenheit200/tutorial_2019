b4509d9faaf09ab5117f7eeab829350706221030
mafp <767102280@qq.com>
2019-09-13 18:37:31 +0800

    task ros

---
+++ docs/exercises/ros_car/build/little_car/CMakeFiles/little_car_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/little_car_node.dir/src/parser.cpp.o"
+  "/home/mafp/build/ros_tutorials/ros_car/devel/lib/little_car/little_car_node.pdb"
+  "/home/mafp/build/ros_tutorials/ros_car/devel/lib/little_car/little_car_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/little_car_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
