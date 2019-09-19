d969448c26892cca7184032d04ef99504171d0f1
LIU-Yinyi <liuyy95@mail2.sysu.edu.cn>
2019-09-19 23:19:31 +0800

    fix bugs for week 1 task

--- task_submit/LIU-Yinyi/week1/senior/src/MaxCommonSuffix.cpp
+++ task_submit/LIU-Yinyi/week1/senior/src/MaxCommonSuffix.cpp
@@ -10,16 +10,25 @@ string longestCommon(const vector<string>& strs)
 		return strs[0];
 
 	string str = strs[0];
+	if(str.size() <= 0)
+		return string();
+
 	int common_count = 0, idx = 0;
 
 	bool isEqual = true;
 	while(isEqual)
 	{
-		idx = str.size() - common_count - 1;
+		int tmp = str.size() - common_count - 1;
+		if(tmp < 0)
+			break;
+		else
+			idx = tmp;
 		char chr = str[idx];
 		for(int i = 1; i < strs.size(); i++)
 		{
 			int idy = strs[i].size() - common_count - 1;
+			if(idy < 0)
+				break;
 			if(chr != strs[i][idy])
 			{
 				isEqual = false;
