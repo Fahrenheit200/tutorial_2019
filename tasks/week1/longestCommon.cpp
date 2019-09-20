bb1c21197d9a614f13cbac05b0288a3cdea5770a
lihzh233 <1061878605@qq.com>
2019-09-20 06:16:21 -0700

    第一周成果

---
+++ tasks/week1/longestCommon.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string longestCommon(vector<string> &strs){
+	string result = "";
+	string min_str = *min_element(strs.begin(), strs.end(),
+			[](const string &s1, const string &s2){return s1.length() < s2.length();});
+	for(unsigned int i = 1; 1 < min_str.length(); i++){
+		for(auto s : strs)
+			if(s[s.length()-i] != min_str[min_str.length()-i]){
+				reverse(result.begin(), result.end());
+				return result;
+			}
+		result += min_str[min_str.length()-i];
+	}
+	reverse(result.begin(), result.end());
+	return result;
+}
+
