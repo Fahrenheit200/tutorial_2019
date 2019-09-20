56b937838ed28b4919c96b8c1f276a3d41b65316
yyava <50123855+yyava@users.noreply.github.com>
2019-09-20 08:54:11 +0800

    Update longest.cpp

--- tasks/week1/programming_exercise/longest.cpp
+++ tasks/week1/programming_exercise/longest.cpp
@@ -2,24 +2,24 @@
 
 string longestCommon(vector<string>& str)
 {
-	//??????????
-	int m = sizeof(str[0]);	//m?????????
-    for (auto v : str) {
+	//寻找输入字符串中最短字符串的长度
+	int m = sizeof(str[0]);	//m用于储存最短字符串的长度
+    for (auto v : str) {	
 		if (sizeof(v) < m) {
 			m = sizeof(v);
 		}
 	}
-	//?????????
-    string ans;		//??????
-    char temp;		//???????i???
+	//找到最大公共后缀
+    string ans;		//ans用于储存结果
+    char temp;		//temp用于暂存倒数第i个字符
     for (int i = 0; i < m ; i++) {		
         temp = str[0][str[0].length() - i - 1];	
         for (auto v : str) {
 			if (temp != v[v.length() - i - 1])	
-				return ans;		//???????ans
+				return ans;	//当不匹配时退出循环，函数返回ans
 		}
 		string t; t += temp;	
-		ans.insert(0, t);		//?str?????????ans???temp
+		ans.insert(0, t);		//将temp插入到ans前
     }
 	return ans;
 }
