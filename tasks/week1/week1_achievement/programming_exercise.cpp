7eb2360d0fb7a107137b21bfc91170e5edb1b2b2
LanYuqiao <lanyq7@mail2.sysu.edu.cn>
2019-09-21 01:38:10 +0800

    完成第一次任务(修改)

--- tasks/week1/week1_achievement/programming_exercise.cpp
+++ tasks/week1/week1_achievement/programming_exercise.cpp
@@ -7,25 +7,37 @@ using namespace std;
 string longestCommon(vector<string>& strs)
 {
     	string longestCommon;							//返回值
-    	int flag ;								//给相同的字符计数
+    	int flag,flag_min  ;							//给相同的字符计数
     	int s = strs.size();							//获取该向量的大小
-    	int len0 = strs[0].length();						//获取第一次输入的字符串大小
-    	for (int i = 0;i < s;i++) {						//遍历这个vector
+    	int len0 = strs[0].length();						//获取第一个字符串的长度
+    	int len1 = strs[1].length();						//获取第二个字符串的长度
+    	for (flag_min = 0;len0-1-flag_min>=0&&len1-1-flag_min>=0; ){		//对比前两个字符串中的公共后缀，并作为基准
+    		if (strs[0][len0-1-flag_min]-strs[1][len1-1-flag_min] == 0){
+    			flag_min++;
+    			continue;    		
+    		}
+    		else break;
+    	}						
+    	for (int i = 1;i < s;i++) {						//遍历这个vector
         	int len = strs[i].length();					//获取每一个字符串的长度
         	flag = 0;							//在遍历一个新的字符串之前将相同字符数重置为0
-        	for (flag = 0;len0-1-flag>=0&&len-1-flag>=0; flag++){		//从字符串的末尾向前遍历
-            		if(strs[i][len-1-flag]-strs[0][len0-1-flag]==0)		//判断两字符是否相等
-                		continue;					//若相当则继续循环
-            			else break;					//若不相等则跳出循环	
+        	for (flag = 0;len0-1-flag>=0&&len-1-flag>=0; ){			//从字符串的末尾向前遍历
+            		if(strs[i][len-1-flag]-strs[0][len0-1-flag]==0){	//判断并计数公共字符
+            			flag++;
+            			continue;
+            		}	
+            		else break;						//若不相等则跳出循环	
         	}
+        	if (flag < flag_min)						//将当前字符串与第一个字符串的公共后缀与前两个字符串的公共后缀比较，取较小的一个
+        		flag_min = flag;
     	}
-    	if(flag==0)								//若无字符相等，则输出空格
+    	if(flag_min==0)								//若无字符相等，则输出空格
         	longestCommon = " ";
     	else{									//有字符相等则将相等的字符串赋值给返回值
-        	string common (strs[0],len0-flag,len0-1);
+        	string common (strs[0],len0-flag_min,len0);
         	longestCommon = common;
     	}
-    	cout << longestCommon << endl;						//输出返回值
+    	cout << longestCommon << endl << "min" << flag_min <<"flag" << flag << endl;						//输出返回值
     	return longestCommon;							//返回值
 }
 int main()
