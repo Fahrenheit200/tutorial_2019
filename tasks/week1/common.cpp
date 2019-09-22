0958eba2f5b0faacbb3ad68a84feb85ae4feb421
jaywhyle <1669974291@qq.com>
2019-09-21 21:41:31 -0700

    完成第一周任务

--- tasks/week1/common.cpp
+++ tasks/week1/common.cpp
@@ -1,30 +1,30 @@
 #include <algorithm>
 string longestCommon(vector <string>& s)
 {
-	if(s.size()==1) {return *s.begin();}
-	else
-	{
-	    int i=0;
-	    int x=0;
-            string s1=*s.begin();          
-	    string s2=*(s.begin()+1);
+        if(s.size()==1) {return *s.begin();}
+        else
+        {
+            int i=0;
+            int x=0;
+            string s1=*s.begin();
+            string s2=*(s.begin()+1);
             reverse(s1.begin(),s1.end());
             reverse(s2.begin(),s2.end());
             char a=s1[0];
             while(i<s1.size()&&i<s2.size())
             {
-    		    if(s1[i]==s2[i]&&s2[i]==a)
+                    if(s1[i]==s2[i]&&s2[i]==a)
                     {
-			    x+=1;
+                            x+=1;
                             i++;
                      }
 
                     else {break;}
              }
-   	    if(x==0) {return "";}
-	    else
-       	    {
-	  	    s.erase(s.begin());
+            if(x==0) {return "";}
+            else
+            {
+                    s.erase(s.begin());
                     s.erase(s.begin());
                     s.insert(s.begin(),string(x,a));
                     return longestCommon(s);
@@ -32,6 +32,4 @@ string longestCommon(vector <string>& s)
         }
 }
 
-
--- VISUAL --                                       1         35,0-1        Bot
 
