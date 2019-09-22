c120fb57f5bfc6a3a1671af3adcbee431ff11f26
Chen XiaoJian <415105654@qq.com>
2019-09-22 21:03:13 +0800

    完成第一周任务

---
+++ tasks/week1/longestCommon.cpp
@@ -0,0 +1,14 @@
+string longestCommon(vector<string> strs){
+  if (strs.size() == 0) return "";
+  for (int i = 0; i < strs[0],length(); i ++){
+	  char c = strs[0].at(strs[0].length() - 1 - i);
+	  for (int j = 1; j < strs.size(); j ++){
+		  if (i == strs[j].length() || strs[j].at(strs[j].length() - 1 - i) != c){
+			  string tmp = strs[0].substr(str[0].length - i, i);
+			  return tmp;
+	          }
+	}
+  }
+  return strs[0];
+}
+
