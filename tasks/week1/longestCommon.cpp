2d96ea06a8d45fc48061bba4bcd5847c471d67f1
张洋铭 <zhangseammm@163.com>
2019-09-22 08:49:13 -0700

    修改了一个文件...

--- tasks/week1/longestCommon.cpp
+++ tasks/week1/longestCommon.cpp
@@ -1,45 +1,43 @@
-#include<string>
-#include<iostream>
+#include <iostream>
+#include<vector>
+#include <string>
 
 using namespace std;
 
-string longestCommon(string a,string b)
+string longestCommon(vector<string> strs)
 {
-	int len_a, len_b;
-	len_a =a.length();
-	len_b =b.length();
-	//长度
-	string back = "";
-	//返回值
+	string back = "";//返回值
+	if (strs.empty())//判断是否为空 空则返回空值
+		return back;
+	int len_a;
+	len_a = strs[0].length();//赋值长度 至少比较两个字符串
+	int i = 1;//作为b 做比较
 	int pa, pb;
-	//作为控制数遍历字符串
-	for (pa = len_a - 1, pb = len_b - 1; (pa >= 0) && (pb >= 0); --pa, --pb)
+
+	for (pa = len_a - 1, pb = strs[i].length() - 1; (pa >= 0) && (pb >= 0); --pa, --pb)
 	{
-		if (a[pa] == b[pb])
+		if (strs[0][pa] == strs[i][pb])
 		{
-			back =a[pa]+back;
+			back = strs[0][pa] + back;
 		}
 		else break;
 	}
-	return back;
+	return back;//输出结果
+
 }
 int main()
 {
-	string a, b;
-	cout << "输入数组" << endl;
-	cin >> a ;
-	cout << "输入第二个数组" << endl;
-	cin >> b;
-	while (cin.get()!=0)
+	vector<string> strs{10};
+	int i = 0;
+	do
 	{
-		a= longestCommon(a,b);
-		cout <<"相同的后缀为："<< a << endl;
-		if (a == "")
-			break;
-		else
-		cout << "输入非0数则视为继续输入数组" << endl;
-		cin >> b;
-	}
-	cout << "无相同后缀默认结束" << endl;
+		strs.reserve(i + 1);
+		cout << "输入第" << i +1<< "个字符串" << endl;
+		cin >> strs[i];
+		cout << "输入回车结束一段字符输入 或只输入0结束输入" << endl;
+		i++;
+	
+	} while (strs[i-1] != "0");
+	cout <<"公共后缀"<< longestCommon(strs)<<endl;
 	system("pause");
 }
