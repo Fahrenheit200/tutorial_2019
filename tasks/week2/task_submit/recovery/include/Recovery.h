b46edb08e41102855adf23761b38ac1bb97b2a11
bbpumpkin <996344439@qq.com>
2019-09-29 22:33:14 +0800

    First complete version

--- tasks/week2/task_submit/recovery/include/Recovery.h
+++ tasks/week2/task_submit/recovery/include/Recovery.h
@@ -1,19 +1,47 @@
 #ifndef _RECOVERY_RECOVERY_H
 #define _RECOVERY_RECOVERY_H
-#include  <chrono>
+#include <vector>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 using namespace cv;
 class Recovery
 {
 public:
+	/* readVideo
+    * @param filePath 文件绝对路径
+    * 根据绝对路径读取需要还原背景的文件
+    */
 	void readVideo(String filePath);
+	/* process
+    * 进行还原过程
+    */
 	void process();
+	/* showResult
+    * 窗口展示背景还原后的结果
+    */
 	void showResult();
 private:
+	void init();
+	void initIterators();
+	void moveIterators();
 	void startClock();
 	float endClock();
+	void saveResult();
+	double gsVec(const Vec3b& x, Vec3f miu, float sga);
+	int getMatchModel(MatIterator_<Vec3b> it);
+	int getBestModel();
+	void resetModel(MatIterator_<Vec3b> it);
+	void updateModel(MatIterator_<Vec3b> it, int bst);
+
+	int fcnt;
+	float rate;
 	VideoCapture cap;
 	Mat frame;
+	Mat result;
+	std::vector<Mat> mius, sigmas, omigas;
 	std::chrono::steady_clock::time_point start_tp;
+	std::vector<MatIterator_<Vec3f>> itus;
+	std::vector<MatIterator_<float>> itas;
+	std::vector<MatIterator_<float>> itos;
 };
 #endif
--- tasks/week2/task_submit/recovery/src/main.cpp
+++ tasks/week2/task_submit/recovery/src/main.cpp
@@ -1,7 +1,13 @@
+#include "Recovery.h"
 #include <opencv2/opencv.hpp>
-using namespace cv;
 
+using namespace cv;
+Recovery rec;
 int main(int argc, char* argv[])
 {
+	rec.readVideo(argv[1]);
+	rec.process();
+	rec.showResult();
+	waitKey(0);
 	return 0;
 }
