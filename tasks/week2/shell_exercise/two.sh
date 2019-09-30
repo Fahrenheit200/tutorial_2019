eb71e7fa5623177937cee4e66905597f979b05fc
Breeze-Chou <46291861+Breeze-Chou@users.noreply.github.com>
2019-09-30 23:42:26 +0800

    第二周成果

---
+++ tasks/week2/shell_exercise/two.sh
@@ -0,0 +1,16 @@
+#author:breeze chou
+#date:2019.9.29
+#version 1.0
+#
+
+
+#!/bin/bash
+
+date
+mkdir /tmp/lstest
+cd /tmp/lstest
+mkdir ald b56e 6test
+touch xy x2y 732
+echo "`ls | grep '^[a|x|6]'`"
+echo "`ls [[:alpha:]][[:digit:]]*`"
+
