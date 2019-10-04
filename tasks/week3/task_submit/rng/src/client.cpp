271eee93ee0c150db0d7e0607a794557eb2fecc5
bbpumpkin <996344439@qq.com>
2019-10-05 00:12:51 +0800

    Build the dynamic reconfigure server for rng package to dynamically adjust the range of the random number

--- tasks/week3/task_submit/rng/src/client.cpp
+++ tasks/week3/task_submit/rng/src/client.cpp
@@ -1,16 +1,23 @@
 #include <cstdlib>
 #include <ctime>
+
 #include "ros/ros.h"
 #include "rng/rng.h"
+
+
 
 int main(int argc, char** argv)
 {
 	std::srand(std::time(0));
 	ros::init(argc, argv, "RNGClient");
 	ros::NodeHandle nh;
+
 	ros::ServiceClient client = nh.serviceClient<rng::rng>("examine_rn");
+	double umin, umax; nh.getParam("/RNGServer/uniform_min", umin); nh.getParam("/RNGServer/uniform_max", umax);
+	ROS_INFO("Get parameters:[%lf, %lf]", umin, umax);
+	float rn = float(std::rand()) / RAND_MAX;
 	rng::rng srv;
-	srv.request.rn = float(std::rand()) / RAND_MAX;
+	srv.request.rn = rn * (umax - umin) + umin;
 	if (client.call(srv))
 	{
 		ROS_INFO("The random number is %f", srv.request.rn);
