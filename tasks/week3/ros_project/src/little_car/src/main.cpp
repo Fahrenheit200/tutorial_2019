802e932332a2a553c14f4b0d2b91bc800054e7d8
蓝钰乔 <lanyq7@mail2.sysu.edu.cn>
2019-10-09 00:52:37 +0800

    解决了上次提交没有解决的CMakeList的BUG

--- tasks/week3/ros_project/src/little_car/src/main.cpp
+++ tasks/week3/ros_project/src/little_car/src/main.cpp
@@ -7,34 +7,38 @@
 #include <ros/ros.h>
 #include <random>
 #include <iomanip>
+#include <cmath>
+#define PI 3.1415926535
+
 using namespace std;
 
+
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "state_publisher");
-    ros::NodeHandle n;
+    	ros::init(argc, argv, "state_publisher");
+    	ros::NodeHandle n;
 	little_car car;//初始化控制对象  小车
-    car.joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    	car.joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
 	car.pos_pub = n.advertise<geometry_msgs::Point>("car_position",1); //小车的位置消息发布
-	/* 
-	 *请添加一个Subscriber，从你自己编写的Publisher处订阅指令
-	 */
-	/*
-	 *若有需要，也可以从小车处发布你所需要的信息
-	 */
-    ros::Rate loop_rate(60);
-	SVector3 velocity={0.8,0.08,0.00};  //速度向量
+	ros::Publisher cmd_pub = n.advertise<geometry_msgs::Point>("vel_cmd",1);
+	geometry_msgs::Point cmd;
+    	ros::Rate loop_rate(60);
+	SVector3 velocity = {0,0,0};  //速度向量
 	car.set_noise_level(0);		   //设置噪声等级
 	float yaw = 0.0;
-    while (ros::ok()) {
-		yaw += 0.01;
-		car.set_yaw(yaw); 		   //修改小车的方向
-		
+	float degree = 0.0;
+    	while (ros::ok()) {
+		cmd.x = 0.05*cos(degree);
+		cmd.y = 0.05;
+		cmd.z = 0.0;
+		degree+=0.05;
+		cmd_pub.publish(cmd);
+		velocity.x = cmd.x;
+		velocity.y = cmd.y;
+		velocity.z = cmd.z;
 		car.set_velocity(velocity);//设置小车速度
-        	car.update_();//小车状态更新
+        	car.update_();//小车状态更新/
 		loop_rate.sleep();
     }
 
-
     return 0;
 }
-
